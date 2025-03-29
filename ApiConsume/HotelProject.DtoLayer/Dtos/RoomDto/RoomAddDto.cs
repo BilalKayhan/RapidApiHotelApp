6e1db90da4881aae100dac921e347aeda12c8dcf
BilalKayhan <fbilalkayhan@gmail.com>
2025-03-29 13:32:21 +0300

    Room dto katmanı oluşturuldu.

---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/RoomAddDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto;
+public class RoomAddDto
+{
+    [Required(ErrorMessage = "Oda Numarası Boş Geçilemez")]
+    public string RoomNumber { get; set; }
+    public string RoomCoverImage { get; set; }
+
+    [Required(ErrorMessage = "Oda Fiyatı Boş Geçilemez")]
+    public int Price { get; set; }
+
+    [Required(ErrorMessage = "Oda Başlığı Boş Geçilemez")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Oda Yatak Kapasitesi Boş Geçilemez")]
+    public string BedCount { get; set; }
+
+    [Required(ErrorMessage = "Oda Banyo Sayısı Boş Geçilemez")]
+    public string BathCount { get; set; }
+    public string Wifi { get; set; }
+    public string Description { get; set; }
+}
---
+++ ApiConsume/HotelProject.DtoLayer/Dtos/RoomDto/UpdateRoomDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelProject.DtoLayer.Dtos.RoomDto;
+public class UpdateRoomDto
+{
+    public int RoomID { get; set; }
+
+    [Required(ErrorMessage = "Oda Numarası Boş Geçilemez")]
+    public string RoomNumber { get; set; }
+
+    [Required(ErrorMessage = "Oda Görseli Boş Geçilemez")]
+    public string RoomCoverImage { get; set; }
+
+    [Required(ErrorMessage = "Oda Fiyatı Boş Geçilemez")]
+    public int Price { get; set; }
+
+    [Required(ErrorMessage = "Oda Başlığı Boş Geçilemez")]
+    [StringLength(100, ErrorMessage = "Oda Başlığı 100 Karakterden Fazla Olamaz")]
+    public string Title { get; set; }
+
+    [Required(ErrorMessage = "Oda Yatak Kapasitesi Boş Geçilemez")]
+    public string BedCount { get; set; }
+
+    [Required(ErrorMessage = "Oda Banyo Sayısı Boş Geçilemez")]
+    public string BathCount { get; set; }
+    public string Wifi { get; set; }
+    public string Description { get; set; }
+}
