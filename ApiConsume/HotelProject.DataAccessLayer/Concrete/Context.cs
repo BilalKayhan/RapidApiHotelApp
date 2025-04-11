d8a2f5733c9d699e4d67598350c05b605bf06154
BilalKayhan <fbilalkayhan@gmail.com>
2025-04-11 15:00:13 +0300

    Booking tablosu hazırlandı.

--- ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
+++ ApiConsume/HotelProject.DataAccessLayer/Concrete/Context.cs
@@ -15,4 +15,5 @@ public class Context : IdentityDbContext<AppUser, AppRole, int>
     public DbSet<Subscribe> Subscribes { get; set; }
     public DbSet<Testimonial> Testimonials { get; set; }
     public DbSet<About> Abouts { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
 }
